7cf5b8d7fad00a23213df87c9ff87b1490b368ac
GAKMYO <fkaynakli@uludag.edu.tr>
2025-11-11 11:59:11 +0300

    HTML yerine PHP dosyalara geçildi

---
+++ iletisim.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'html_header.php';
+?>
+<h1>İletişim</h1>
+<?php
+require_once 'html_footer.php';
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'html_header.php';
+?>
+<h1>Gemlik Sunğipek Kampüsüne Hoşgeldiniz</h1>
+<?php
+require_once 'html_footer.php';
