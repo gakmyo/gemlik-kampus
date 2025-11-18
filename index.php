5880ba580c171463a8f6a20797076bef12381faf
GAKMYO <fkaynakli@uludag.edu.tr>
2025-11-18 11:07:26 +0300

    Ana sayfa stili ile ilgili hata giderildi

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 
 require_once 'html_header.php';
 ?>
-<h1>Gemlik Sunğipek Kampüsüne Hoşgeldiniz</h1>
+<h1 class="container">Gemlik Sunğipek Kampüsüne Hoşgeldiniz</h1>
+<p class="alert alert-success">Yeni kayıt başarıyla tamalandı!</p>
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    Kayıt sırasında hata oluştu: Ürün kodu zorunlu
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
 <?php
 require_once 'html_footer.php';
