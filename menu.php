c7e7e7e187a09f3f32929c1c14ce3a45a5a8ec1b
GAKMYO <fkaynakli@uludag.edu.tr>
2025-11-18 11:13:47 +0300

    Ana menü tamamlandı

--- menu.php
+++ menu.php
@@ -1,5 +1,26 @@
-<ul>
+<!-- <ul>
     <li><a href="/">Ana Sayfa</a></li>
     <li><a href="kampus.php">Kampüsümüz</a></li>
     <li><a href="iletisim.php">İletişim</a></li>
-</ul>
+</ul> -->
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">GAKMYO</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/">Ana Sayfa</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="kampus.php">Kampüsümüz</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="iletisim.php">İletişim</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
